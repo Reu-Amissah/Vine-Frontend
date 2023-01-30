62d58d5117038c7bad7ccc07a6ae36d8e43e76f0
Reu-Amissah <reujoyamissah@gmail.com>
2023-01-30 15:17:29 +0000

    add navbar.css

--- vine-app/src/components/Navbar/Navbar.js
+++ vine-app/src/components/Navbar/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='nav-container'>
+        <div className='logo'>
+          Logo
+        </div>
+        <ul className='link-items'>
+          <li><a href="#about">Home</a></li>
+          <li><a href="#services">About Us</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+          <li><a href="#contact">Enrolment</a></li>
+        </ul>
+        <div className='buttons'>
+          Buttons
+        </div>
+      </div>
+      
+    </nav>
   )
 }
 
