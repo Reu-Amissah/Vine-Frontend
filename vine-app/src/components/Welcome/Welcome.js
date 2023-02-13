785a2e3e621c3bb1ad6edc3466898bf9f918e7d7
Reu-Amissah <reujoyamissah@gmail.com>
2023-02-13 23:38:18 +0000

    add img, text and styling to welcome component

--- vine-app/src/components/Welcome/Welcome.js
+++ vine-app/src/components/Welcome/Welcome.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "./Welcome.css";
-import welcomeImage from "";
+import welcomeImage from "../../assets/descriptionImg.svg";
+import Button from "../Buttons/Button";
 
 const Welcome = () => {
   return (
-    <div>
-      <div>
-        <p>Ghana’s #1 Basic School with top notch educational Facilities</p>
-      </div>
-      <div>
-        <img src={welcomeImage} alt="welcomeImage" />
+    <div className="welcome-container">
+      <div className="container-welcome">
+        <div className="welcome-text">
+          <h1>
+            Ghana’s #1 Basic <br />
+            School with top notch <br />
+            educational Facilities
+          </h1>
+          <p>
+            Enroll your children into the hands of well <br />
+            trained professional tutors in Ghana
+          </p>
+          <Button title={"Get Started"} cName={"get-started"}></Button>
+        </div>
+
+        <div className="welcome-img-container">
+          <img src={welcomeImage} alt="welcomeImage" className="welcome-img" />
+        </div>
       </div>
     </div>
   );
