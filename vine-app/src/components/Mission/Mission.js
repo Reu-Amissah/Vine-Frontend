84367d80ccdb67df62b66fe1d621d7c34f33a5a6
Reu-Amissah <reujoyamissah@gmail.com>
2023-02-14 00:01:44 +0000

    add mission component

---
+++ vine-app/src/components/Mission/Mission.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Mission.css";
+
+const Mission = () => {
+  return (
+    <div>
+      <div>
+        <div className="mission-title">
+          <h3>Mission</h3>
+          <span className="mission-span"></span>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
+          lectus. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Mission;
