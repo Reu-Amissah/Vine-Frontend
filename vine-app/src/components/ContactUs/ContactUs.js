f11ec8281f9cfaac0a213a3dfa7c147e06a3e681
Reu-Amissah <reujoyamissah@gmail.com>
2023-02-15 16:22:49 +0000

    add contact us form

---
+++ vine-app/src/components/ContactUs/ContactUs.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./ContactUs.css";
+import ContactDetails from "../ContactDetails/ContactDetails";
+import {
+  faPhone,
+  faLocation,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us-container">
+      <div className="container-contact-us">
+        <div className="contact-us-header-text">
+          <h2>Get in touch!</h2>
+          We'd like to hear from you. Make enquiries below
+        </div>
+
+        <div className="contact-details-section">
+          <ContactDetails
+            text={"+233 243 7777 61"}
+            iconName={faPhone}
+          ></ContactDetails>
+          <ContactDetails
+            text={"Madina-Accra, Ghana"}
+            iconName={faLocation}
+          ></ContactDetails>
+          <ContactDetails
+            text={"reujoyamissah@gmail.com"}
+            iconName={faEnvelope}
+          ></ContactDetails>
+        </div>
+
+        <div className="contact-form">
+          <div className="contact-form-container">
+            <div className="contact-form">Contact Form</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
