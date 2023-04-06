b9abfe9ec784e801b1bcdefb84d98beb3168f803
Reu-Amissah <reujoyamissah@gmail.com>
2023-04-06 23:12:14 +0000

    add footer

---
+++ vine-app/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        Copyright &copy; 2023 Vine's Care Preparatory
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
